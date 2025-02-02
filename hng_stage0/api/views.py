4aa7bccf7b09e14e349b74c165c42eab668f1b43
clencyc <clency2023@gmail.com>
2025-02-02 15:57:20 +0300

    Finished the api setup

--- hng_stage0/api/views.py
+++ hng_stage0/api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# view to handle GET request and return the required JSON response.
+
+from django.http import JsonResponse
+from datetime import datetime
+
+def get_info(request):
+    if request.method == 'GET':
+        data = {
+            "email": "christineoyiera51@gmail.com",
+            "current_datetime": datetime.utcnow().isoformat() + "Z",
+            "github_url": "https://github.com/clencyc/Backend-HNG"
+        }
+        return JsonResponse(data, status=200)
+    
+if __name__ == "__main__":
+    app.run(debug=True)
