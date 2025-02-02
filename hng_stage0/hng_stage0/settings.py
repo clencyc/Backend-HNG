1da81f4c53be55771d7b6721e75e8b5a9737c807
clencyc <clency2023@gmail.com>
2025-02-02 19:18:33 +0300

    Changed settings

--- hng_stage0/hng_stage0/settings.py
+++ hng_stage0/hng_stage0/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-aepu*8se4^hugtxwt^^pu0w@bq^rejsx$9v!b84&f(jrql)e(q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+
 
 
 # Application definition
